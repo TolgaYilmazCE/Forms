3ef738fd7e041fde121c20e7a6ef50c69d3b48af
Toniii10 <1501024003@stu.iku.edu.tr>
2021-03-20 11:37:21 +0300

    first forms commit

---
+++ form5.php
@@ -0,0 +1,16 @@
+<?php
+$old_entry=isset($_POST['input'])?$_POST['input']:'';
+?>
+<h1> HTML Forms</h1>
+<form method="POST">
+    <p><label for="input">User input</label>
+        <input type="text" name="input" size="40" value="<?=htmlentities($old_entry) ?>"/>
+    </p>
+    <input type="submit"/>
+</form>
+<pre>
+$_POST:
+<?php
+print_r($_POST);
+?>
+</pre>
